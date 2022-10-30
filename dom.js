fedb712942890d8c75a472611eddcfde5a36bc2a
ndarkhan9 <darkhan2123@gmail.com>
2022-10-30 16:28:57 +0100

    Created div, appended h1 and p tags before adding it to container

--- dom.js
+++ dom.js
@@ -22,3 +22,22 @@ hhh.appendChild(node2);
 const element2 = document.getElementById("container");
 element2.appendChild(hhh);
 
+
+const cont = document.querySelector("#container");
+const content3 = document.createElement("div");
+content3.setAttribute("id", "hello");
+content3.style.borderColor = "black";
+content3.style.backgroundColor = "pink";
+cont.appendChild(content3);
+const h = document.createElement("h1");
+const node3 = document.createTextNode("I'm in a div");
+h.appendChild(node3);
+const element3 = document.getElementById("hello");
+element3.appendChild(h);
+const para2 = document.createElement("p");
+const node4 = document.createTextNode("ME TOO!");
+para2.appendChild(node4);
+const element4 = document.getElementById("hello");
+element4.appendChild(para2);
+
+
